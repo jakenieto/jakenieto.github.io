fda12fb20352026e0102ba932ea1c2dd2071c240
Jake Nieto <jnieto@Jakes-MacBook-Pro.local>
2019-12-20 23:30:02 -0500

    actual working mixtape-live, adam is wrong

---
+++ build/precache-manifest.5a3c31ce4fbba72ca31db43a542a7a79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf2f9cfe1eb1906fc4cf056bae1e70d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "129f2bb94396e864677a",
+    "url": "./static/css/2.aa30e9ed.chunk.css"
+  },
+  {
+    "revision": "508359792ba84ba28dec",
+    "url": "./static/css/main.43d46cdf.chunk.css"
+  },
+  {
+    "revision": "129f2bb94396e864677a",
+    "url": "./static/js/2.e87ff0dc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.e87ff0dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "508359792ba84ba28dec",
+    "url": "./static/js/main.ade6e301.chunk.js"
+  },
+  {
+    "revision": "b238d64db016bba6d2fa",
+    "url": "./static/js/runtime-main.769fe074.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
